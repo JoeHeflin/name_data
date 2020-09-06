83b7e8a9ac0132ef8a64d83bc7fbeedb02234b8f
Joe Heflin <jnh24@duke.edu>
2020-09-06 19:16:56 -0400

    refactored avgrank finder method

--- src/names/Complete5.java
+++ src/names/Complete5.java
@@ -1,10 +1,26 @@
 package names;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Complete5 {
-    public static List<String> run(int[] years, String gender) {
+    public static List<String> run(int[] years, String gender) throws Exception {
+        NameArray namesInFirstYear = new NameArray(gender, years);
+        int maxAvgRankVal = 0;
+        List<String> maxAvgRankList = new ArrayList<>();
 
-        return null;
+        for(Name name:namesInFirstYear.nameArray){
+            int avgRank = Main.findAverageRank(years,gender,name.name);
+            if(avgRank == maxAvgRankVal){
+                maxAvgRankList.add(name.name);
+            }
+            else if(avgRank > maxAvgRankVal){
+                maxAvgRankList.clear();
+                maxAvgRankList.add(name.name);
+                maxAvgRankVal = avgRank;
+            }
+        }
+        maxAvgRankList.sort(String::compareTo);
+        return maxAvgRankList;
     }
 }
--- src/names/Main.java
+++ src/names/Main.java
@@ -11,4 +11,21 @@ public class Main {
     public static void main (String[] args) {
 
     }
+
+    public static int findAverageRank(int[] years, String gender, String name) throws Exception {
+        int totalRank = 0;
+
+        if(years[1]<years[0]){
+            throw new Exception("Invalid Year Range");
+        }
+
+        for(int year = years[0]; year <= years[1]; year++){
+            NameArray n = new NameArray(gender,year);
+            int rank = n.findRank(name);
+            totalRank += rank;
+        }
+        int avgRank = totalRank/(years[1]-years[0]+1);
+
+        return avgRank;
+    }
 }
